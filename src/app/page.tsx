4d74609c91cc251a277f4137c00d6439a5c2c424
Suraj Kumar <nehalkumarsuraj@gmail.com>
2023-12-09 13:18:12 +0800

    Somewhat there

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
-'use client'
-import {Navbar} from "@/components/website/navbar"
+
+import { Metadata } from "next"
+import Link from "next/link"
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+export const metadata: Metadata = {
+  title: "Farrer Park Hospital",
+  description: "Sign Up as a nurse today!",
+}
+
 export default function Home() {
   return (
-    <div className="justify-center px-10 py-10">
-      <Navbar />
+    <>
+    <div className="flex flex-col space-y-4 ">
+    <Button> <Link href="/Auth/Login">Login</Link></Button>
+    <Button><Link href="/Auth/SignUp">Sign Up</Link></Button>
+    <Button><Link href="/Dashboard">Dashboard</Link></Button>
+    <Button><Link href="/Cards">Cards</Link></Button>
+    <Button><Link href="/Tasks">Tasks</Link></Button>
+    <Button><Link href="/Forms">Forms</Link></Button>
     </div>
+    </>
   )
 }
