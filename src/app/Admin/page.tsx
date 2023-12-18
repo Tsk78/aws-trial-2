aad7c9e3f328e2404c405da3dd84046b10bffe9b
Rameshkumar Dhanvine <rdhanvine321@gmail.com>
2023-12-18 21:29:50 +0800

    actual legit admin page

--- src/app/Admin/page.tsx
+++ src/app/Admin/page.tsx
@@ -1,41 +1,29 @@
-"use client"
+'use client';
 import React, { useState } from 'react';
-import nurses from './Jobs/data/nurses.json'; // adjust the path to your nurses.json file
-import { UserNav } from "./components/user-nav"
-import { DataTable } from "./components/data-table"
-import { MainNav } from '@/components/website/main-nav';
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { MainNav } from "@/components/website/main-nav"
+import { UserNav } from "@/components/website/user-nav"
 
-export default function NurseApplicantsPage() {
-  const [jobs, setJobs] = useState([]);
+interface Job {
+  title: string;
+  applicants: number;
+}
+
+export default function AdminPage() {
+  const [jobs, setJobs] = useState<Job[]>([]);
   const [newJobTitle, setNewJobTitle] = useState('');
 
-  const columns = React.useMemo(
-    () => [
-      {
-        id: 'name',
-        Header: 'Name',
-        accessor: 'Name',
-      },
-      {
-        id: 'typeOfRegistration',
-        Header: 'Type of Registration',
-        accessor: 'Type_of_Registration',
-      },
-      {
-        id: 'nric',
-        Header: 'NRIC',
-        accessor: 'NRIC',
-      },
-      // Add more columns as needed
-      // ...
-      {
-        id: 'locumExperienceRating',
-        Header: 'Locum Experience Rating',
-        accessor: 'Locum_Experience_rating',
-      },
-    ],
-    []
-  );
+  const handleAddJob = () => {
+    setJobs([...jobs, { title: newJobTitle, applicants: 0 }]);
+    setNewJobTitle('');
+  };
 
   const handleRemoveJob = (index: number) => {
     const newJobs = [...jobs];
@@ -63,10 +51,27 @@ export default function NurseApplicantsPage() {
               onChange={(e) => setNewJobTitle(e.target.value)}
               placeholder="Job title"
             />
+            <Button onClick={handleAddJob}>Add job</Button>
           </div>
         </div>
-        <DataTable columns={columns} data={nurses} />
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+          {jobs.map((job, index) => (
+            <Card key={index}>
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">{job.title}</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="text-2xl font-bold">Applicants: {job.applicants}</div>
+                <Button>View applicants</Button>
+                <Button onClick={() => handleRemoveJob(index)}>Remove job</Button>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
       </div>
     </>
   );
 }
+
+
+
